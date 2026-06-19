f2e92369eca4b5b5d8678571efb127d8bbad18f1
Vlad Lozhenkov <vladlozhenkov1997@gmail.com>
2026-06-19 22:35:11 +0300

    Learn errors, switch, defer, structs, enums fiinish quizes

---
+++ src/exercices/026_hello.zig
@@ -0,0 +1,51 @@
+//
+// Great news! Now we know enough to understand a "real" Hello World
+// program in Zig - one that uses the system Standard Out resource...which
+// can fail!
+//
+const std = @import("std");
+const MainError = error{
+    ToSmall,
+    ToBigg,
+};
+// Take note that this main() definition now returns "!void" rather
+// than just "void". Since there's no specific error type, this means
+// that Zig will infer the error type. This is appropriate in the case
+// of main(), but can make a function harder (function pointers) or
+// even impossible to work with (recursion) in some situations.
+//
+// You can find more information at:
+// https://ziglang.org/documentation/master/#Inferred-Error-Sets
+//
+pub fn main(init: std.process.Init) !void {
+    // Instance for input/output operations; we will learn more about this later.
+    const io = init.io;
+
+    // We get a Writer for Standard Out...
+    var stdout_writer = std.Io.File.stdout().writer(io, &.{});
+    // ...and extract its interface so we can print() to it.
+    const stdout = &stdout_writer.interface;
+
+    const result = try createError(10);
+
+    try stdout.print("the result is: {}\n", .{result});
+
+    // Unlike std.debug.print(), the Standard Out writer can fail
+    // with an error. We don't care _what_ the error is, we want
+    // to be able to pass it up as a return value of main().
+    //
+    // We just learned of a single statement which can accomplish this.
+    return try stdout.print("Hello world!\n", .{});
+}
+
+fn createError(n: u32) MainError!u32 {
+    if (n < 10) {
+        return MainError.ToSmall;
+    }
+
+    if (n > 10) {
+        return MainError.ToBigg;
+    }
+
+    return n;
+}
