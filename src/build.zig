c494ab3904caf65d0eb211a6c65f05b42a8deab0
Vlad Lozhenkov <vladlozhenkov1997@gmail.com>
2026-07-14 23:44:48 +0300

    Creating build.zig to able run file with cImport explicit specified the application binary interface

--- src/build.zig
+++ src/build.zig
@@ -1,13 +1,18 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{
+        .default_target = .{ .abi = .musl },
+    });
+
     const exe = b.addExecutable(.{
         .name = "cImports",
         .root_module = b.createModule(.{
-            .root_source_file = b.lazyImport(comptime asking_build_zig: type, comptime dep_name: []const u8)"/exercices/096_hello_c.zig",
-            .target = b.graph.host,
+            .root_source_file = b.path("exercices/096_hello_c.zig"),
+            .target = target,
+            .link_libc = true,
         }),
     });
 
-    b.installArtifact(b, exe);
+    b.installArtifact(exe);
 }
